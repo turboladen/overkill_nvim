199f44b080c1cab508776fd1af862f3214f1e3bc
Steve Loveless <steve.loveless@gmail.com>
2021-12-09 13:50:52 -0800

    gh-6 Add nvim_test-proc_macro
    Will probably need some improvements, but this makes writing tests a lot more bearable.
    
    Closes gh-6.

--- nvim_test-proc_macro/src/lib.rs
+++ nvim_test-proc_macro/src/lib.rs
@@ -1,8 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn nvim_test(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(item as syn::ItemFn);
+    let fn_name = input.sig.ident.clone();
+    // input.sig.ident = Ident::new(&format!("__{}", input.sig.ident), Span::call_site());
+    let block = input.block;
+
+    let test_code = quote::quote! {
+        #[allow(box_pointers)]
+        #[no_mangle]
+        pub extern "C" fn #fn_name() -> bool {
+            std::panic::set_hook(Box::new(|panic_info| {
+                if let Some(location) = panic_info.location() {
+                    eprintln!(
+                        "panic occurred in file '{}' at line {}",
+                        location.file(),
+                        location.line()
+                    );
+                } else {
+                    eprintln!("panic occurred but can't get location information...");
+                }
+
+                if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
+                    eprintln!("FAIL: {}", s);
+                    return;
+                }
+
+                if let Some(s) = panic_info.payload().downcast_ref::<String>() {
+                    eprintln!("FAIL: {}", s);
+                }
+            }));
+
+            let result = std::panic::catch_unwind(|| #block);
+
+            result.is_ok()
+        }
+    };
+
+    test_code.into()
 }
